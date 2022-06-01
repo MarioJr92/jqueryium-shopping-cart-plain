88e62e15b34995295ff097a7df670123915db206
MarioJr92 <mariorodjr92@gmail.com>
2022-05-31 22:51:11 -0400

    shopping cart project complete

--- js/application.js
+++ js/application.js
@@ -31,3 +31,34 @@ const updateTotal = () => {
   let totalCost = updateTotalCost().reduce(sum).toFixed(2);
   $('#totalCost').html(totalCost);
 };
+
+$(document).ready(function() {
+  updateTotal();
+
+  let timeout;
+  $(document).on('input', 'tr input', function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      updateTotal();
+    }, 800);
+  });
+
+  $(document).on('click', '.remove', function(event) {
+    $(this).closest('tr').remove();
+    updateTotal();
+  });
+
+  $('#itemToAdd').on('submit', function(event) {
+    event.preventDefault();
+    let jersey = $(this).children('[name=jerseyName]').val();
+    let price = parseFloat($(this).children('[name=price]').val()).toFixed(2);
+
+    $('tbody').append(`<tr><td class="jersey">${jersey}</td><td class="price">$${price}</td><td class="quantity"><input type="number" min="0" max="20"></td><td class="cost"></td><td><button class="remove">Remove</button></td></tr>`
+    );
+
+    updateTotal();
+    $(this).children('[name=jerseyName]').val('');
+    $(this).children('name=price').val('');
+  });
+
+});
